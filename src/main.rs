f987374b6af24a1a7380cc84d3fc3681fdb69839
shamanskiy <shamanskiy.alexander@gmail.com>
2023-11-12 20:53:15 +0100

    implement sky gradient

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+mod ray;
+
 use std::io;
 
+use cgmath::{InnerSpace, Vector3};
 use image::{ImageBuffer, Rgba, RgbaImage};
+
+use crate::ray::Ray;
 
 fn main() -> io::Result<()> {
     let img: RgbaImage = generate_image();
@@ -12,12 +17,34 @@ fn main() -> io::Result<()> {
 }
 
 fn generate_image() -> RgbaImage {
-    let mut img: RgbaImage = ImageBuffer::new(4, 4);
+    let width = 200;
+    let height = 100;
+    let upper_left_corner: Vector3<f32> = Vector3::new(-2., 1., -1.);
+    let origin: Vector3<f32> = Vector3::new(0., 0., 0.);
+    let horizontal_span: Vector3<f32> = Vector3::new(4., 0., 0.);
+    let vertical_span: Vector3<f32> = Vector3::new(0., -2., 0.);
+
+    let mut img: RgbaImage = ImageBuffer::new(width, height);
     for (x, y, pixel) in img.enumerate_pixels_mut() {
-        let red: u8 = if (x + y) % 4 == 3 { 255 } else { 0 };
-        let green: u8 = if x % 2 == 1 { 255 } else { 0 };
-        let blue: u8 = if y % 2 == 1 { 255 } else { 0 };
-        *pixel = Rgba([red, green, blue, 255]); // Red color in RGBA format
+        let u = x as f32 / width as f32;
+        let v = y as f32 / height as f32;
+        let direction = upper_left_corner + u * horizontal_span + v * vertical_span;
+        let ray = Ray::new(origin, direction);
+        *pixel = color_ray(&ray)
     }
     return img;
+}
+
+fn color_ray(ray: &Ray) -> Rgba<u8> {
+    let normalized_direction = ray.direction.normalize();
+    let blend_factor = 0.5 * (normalized_direction.y + 1.);
+    let white = Vector3::new(1., 1., 1.);
+    let light_blue = Vector3::new(0.5, 0.7, 1.0);
+
+    let color = (1. - blend_factor) * white + blend_factor * light_blue;
+
+    let red = (color.x * 255.) as u8;
+    let green = (color.y * 255.) as u8;
+    let blue = (color.z * 255.) as u8;
+    return Rgba([red, green, blue, 255]);
 }
---
+++ src/ray.rs
@@ -0,0 +1,18 @@
+use cgmath::Vector3;
+
+// allows using println!("{:?}", ray);
+#[derive(Debug)]
+pub struct Ray {
+    pub origin: Vector3<f32>,
+    pub direction: Vector3<f32>,
+}
+
+impl Ray {
+    pub fn new(origin: Vector3<f32>, direction: Vector3<f32>) -> Self {
+        Self { origin, direction }
+    }
+
+    pub fn eval(self, t: f32) -> Vector3<f32> {
+        return self.origin + self.direction * t;
+    }
+}
